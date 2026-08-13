1d22175893dd1cc1156ab9f3c91b24041e345c28
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-13 07:31:26 +0530

    calculate the salary from weekly working hours in the list

---
+++ 06_list/03_calculatesalary.py
@@ -0,0 +1,9 @@
+"calculate salary,weekly working hours given in the list.per hours earns 70 Rs"
+
+work_hours = [7,8,7,9,10]
+
+total_hours = 0
+for total_hours in work_hours:
+    total_hours+=total_hours
+salary = total_hours*70
+print(salary)
