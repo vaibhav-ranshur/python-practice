fee2b532f0471466938b70ef1a23b413f9733681
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-13 07:01:09 +0530

    calculate sum of elements

---
+++ 06_list/01_sumofthelist.py
@@ -0,0 +1,11 @@
+"""
+Write a Python program to calculate and print the sum of all elements in the list.
+"""
+
+
+numbers = [10, 20, 30, 40, 50]
+
+addition = 0
+for x in numbers:
+   addition = addition + x
+print(addition)
