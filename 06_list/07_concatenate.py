d1f5b0a49797f6cd622a139516dc8200d78794e3
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-14 07:04:10 +0530

    concatenate all integers from a list to a single number

---
+++ 06_list/07_concatenate.py
@@ -0,0 +1,17 @@
+"""
+concatenate all integers from a list to a single number
+"""
+
+l1 = [5,2,8,7,6]
+res=""
+for x in l1:
+    res += str(x)
+print(res)
+
+"""
+result = ""
+for i in range(len(l1)):
+    result = result + str(l1[i])
+print(result)
+
+"""
