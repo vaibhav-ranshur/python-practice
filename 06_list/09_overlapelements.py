a94c6431dbf0a0a8abf5850a959be0325c58aeac
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-16 01:42:18 +0530

    finding overlapping elements and append overlapped ele to new list

--- 06_list/09_overlapelements.py
+++ 06_list/09_overlapelements.py
@@ -6,8 +6,7 @@ l1 = [1,5,3,4,9,7]
 l2 = [5,2,8,9,1,6,7]
 
 l3 = []
-for i in range(0,len(l1)):
-    if l1[i] in l2:
-        l3.append(l1[i])
+for x in l1 :
+    if x in l2:
+        l3.append(x)
 print(l3)
-
