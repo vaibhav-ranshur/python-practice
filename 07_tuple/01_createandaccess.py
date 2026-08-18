733ae8f99553e0644d51dbe69d747a9c61738271
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-19 02:44:01 +0530

    Add tuple question 1 and gitignore

---
+++ 07_tuple/01_createandaccess.py
@@ -0,0 +1,18 @@
+"""
+Q1. Create and access a tuple
+
+Create a tuple containing:
+10, 20, 30, 40, 50
+
+Print:
+
+The complete tuple
+First element
+Last element
+Third element
+"""
+t1 = (10,20,30,40,50)
+print(t1)
+print(t1[0])
+print(t1[len(t1)-1])
+print(t1[2])
