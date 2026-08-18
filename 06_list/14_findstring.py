ae4579fcfe8b87c5f9a1dc33fb4255be5043570d
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-18 10:59:32 +0530

    find the word starting with the given letter in the list

--- 06_list/14_findstring.py
+++ 06_list/14_findstring.py
@@ -5,6 +5,12 @@ find the word starting with the given letter in the list
 food = ["pizza","nuggets","hotdog","noodles","pasta","burger"]
 
 letter = input("enter the letter").lower()
+found = "false"
+
 for x in food:
      if x.startswith(letter):
          print(x)
+         found = "true"
+
+if not found:
+    print("not found")
