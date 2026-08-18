89aa80506dffe3fe0fcd4bc63fa6d1c066572cfe
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-19 03:11:56 +0530

    slice element from tuple

---
+++ 07_tuple/02_slicing.py
@@ -0,0 +1,18 @@
+"""
+Given:
+numbers = (10, 20, 30, 40, 50, 60, 70, 80)
+
+Print:
+The first 4 elements
+The last 3 elements
+Elements from index 2 to index 5
+The tuple in reverse order
+Every second element
+"""
+
+numbers = (10, 20, 30, 40, 50, 60, 70, 80)
+print(numbers[:4])
+print(numbers[-3:])
+print(numbers[2:6])
+print(numbers[::-1])
+print(numbers[::2])
