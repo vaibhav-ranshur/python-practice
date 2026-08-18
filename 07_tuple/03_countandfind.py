72e50fccb280e0fe61f55e7bbacf27da639b7d36
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-19 03:24:39 +0530

    count and find element of a tuple

---
+++ 07_tuple/03_countandfind.py
@@ -0,0 +1,14 @@
+"""
+Given:
+
+numbers = (10, 20, 30, 20, 40, 20, 50, 30)
+
+Write code to:
+Count how many times 20 appears.
+Find the index of the first occurrence of 30.
+Find the index of the first occurrence of 50.
+"""
+numbers = (10, 20, 30, 20, 40, 20, 50, 30)
+print(numbers.count(20))
+print(numbers.index(30))
+print(numbers.index(50))
