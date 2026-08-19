87569118b0e9c25ca8df9053aa76f927dbe94088
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-19 08:31:19 +0530

    accessing the element of nested tuple

---
+++ 07_tuple/06_nestedtuple.py
@@ -0,0 +1,28 @@
+"""
+Given:
+
+data = (
+    ("Alice", 25),
+    ("Bob", 30),
+    ("Charlie", 28)
+)
+Task
+
+Print:
+
+The complete data
+"Alice"
+25
+"Charlie"
+28
+"""
+data = (
+    ("Alice", 25),
+    ("Bob", 30),
+    ("Charlie", 28)
+)
+print(data)
+print(data[0][0])
+print(data[0][1])
+print(data[2][0])
+print(data[2][1])
