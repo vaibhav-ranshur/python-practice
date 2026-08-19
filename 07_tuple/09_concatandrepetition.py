7512bc734b5fd60ee8dc499a899159f1ba0931c4
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-19 09:15:33 +0530

    Practice tuple concatenation and repetition

---
+++ 07_tuple/09_concatandrepetition.py
@@ -0,0 +1,22 @@
+"""
+Given:
+
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+
+Create a new tuple that contains:
+
+(1, 2, 3, 4, 5, 6)
+
+Then create another tuple by repeating t1 3 times:
+
+(1, 2, 3, 1, 2, 3, 1, 2, 3)
+"""
+
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+
+t3 = t1 + t2
+t4 = t1*3
+print(t3)
+print(t4)
