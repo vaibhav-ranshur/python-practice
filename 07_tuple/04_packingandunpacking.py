ad095f42b4ef55b6ac1978a3423d08d5f47af140
Vaibhav Ranshur <vaibhavranshur.data@gmail.com>
2026-08-19 08:13:02 +0530

    unpack given tuple and print each element

---
+++ 07_tuple/04_packingandunpacking.py
@@ -0,0 +1,19 @@
+"""
+Given:
+
+student = ("Vaibhav", 25, "Data Engineering")
+Task
+
+Unpack the tuple into three separate variables:
+
+name
+age
+career
+
+Then print each variable.
+"""
+student = ("Vaibhav", 25, "Data Engineering")
+name,age,career = student
+print(name)
+print(age)
+print(career)
